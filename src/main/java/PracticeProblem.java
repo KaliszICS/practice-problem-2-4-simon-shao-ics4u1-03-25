9c2cadc0bdee5b42465940250ed43e5713e3849b
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-03-21 17:13:09 +0000

    Complete Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,38 @@ public class PracticeProblem {
 
 	}
 
-	
+public static void selectionSortName(String[] names, int[] ages){
+	for (int j = 0; j<names.length-1;j++){
+		int smallestIn = j;
+		for (int i = j+1; i<names.length;i++){
+			if (names[smallestIn].compareToIgnoreCase(names[i])>0){
+				smallestIn = i;
+			}
+		}
+		String temp = names[smallestIn];
+		int tempAge = ages[smallestIn];
+		ages[smallestIn] = ages[j];
+		names[smallestIn] = names[j];
+		names[j] = temp;
+		ages[j] = tempAge;
+	}
+}
+
+public static void selectionSortAge(String[] names, int[] ages){
+	for (int j = 0; j<ages.length-1;j++){
+		int smallestIn = j;
+		for (int i = j+1; i<ages.length;i++){
+			if (ages[smallestIn] > ages[i]){
+				smallestIn = i;
+			}
+		}
+		String temp = names[smallestIn];
+		int tempAge = ages[smallestIn];
+		ages[smallestIn] = ages[j];
+		names[smallestIn] = names[j];
+		names[j] = temp;
+		ages[j] = tempAge;
+	}
+}
 
 }
